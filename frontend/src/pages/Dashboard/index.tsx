2e02f8e1edc0c90073fe798343a18d38fbd20981
thiagofd <thiago.fdornelles@gmail.com>
2020-03-22 01:06:32 -0300

    frontend: dashboard page dispatch tools

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+
+import { State } from '../../interface';
+import { toolsRequest } from '../../store/modules/tools/actions';
 
 import { Container } from './styles';
 
 export default function Dashboard() {
+  const dispatch = useDispatch();
+  const tools = useSelector((state: State) => state.tools.tools);
+  const loading = useSelector((state: State) => state.loading);
+
+  const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    dispatch(toolsRequest());
+    toast.success('Ola');
+  }, []);
+
   return (
     <Container>
       <h1>Tools</h1>
+      {tools?.map(tool => (
+        <h1>{tool.title}</h1>
+      ))}
     </Container>
   );
 }
