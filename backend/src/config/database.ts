736738157058894aa9116adf7391882699801b2b
thiagofd <thiago.fdornelles@gmail.com>
2020-04-01 12:14:40 -0300

    backend: set docker-compose

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,10 +1,22 @@
 /* eslint-disable prettier/prettier */
 import '../bootstrap';
 
-import { getConnectionOptions, createConnection, getConnection } from 'typeorm';
+import {
+  getConnectionOptions, createConnection, getConnection, ConnectionOptions
+} from 'typeorm';
 
 export const createTypeormConn = async () => {
-  const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
+  let connectionOptions: ConnectionOptions;
+
+  if (process.env.NODE_ENV === 'development') {
+    connectionOptions = {
+      ...(await getConnectionOptions(process.env.NODE_ENV)),
+      host: process.env.DB_HOST
+    } as ConnectionOptions;
+  } else {
+    connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
+  }
+
   return createConnection({ ...connectionOptions, name: 'default' });
 };
 
